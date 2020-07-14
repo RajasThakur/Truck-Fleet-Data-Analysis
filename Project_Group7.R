8ec9549e8d02c34478cdf9ae4d137b917fab8961
Rajas Prasannakumar Thakur <65140366+RajasThakur@users.noreply.github.com>
2020-07-14 00:11:43 -0500

    Add files via upload

---
+++ Project_Group7.R
@@ -0,0 +1,67 @@
+# Using Logistic Regression
+
+# Importing the dataset
+dataset = read.csv('risk.csv')
+dataset$riskfactor = ifelse(dataset$riskfactor>=7,1,0)
+
+# Encoding the target feature(Riskfactor) as factor
+dataset$riskfactor = factor(dataset$riskfactor, levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$riskfactor, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+# Logistic Regression
+model = glm(riskfactor~events+totmiles,family=binomial,data = training_set)
+summary(model)
+
+# Predicting the Test set results
+prob_pred = predict(model, type = 'response', newdata = test_set[-3])
+y_pred = ifelse(prob_pred > 0.5, 1, 0)
+
+# Making the Confusion Matrix
+cm = table(test_set[,3], y_pred > 0.5)
+cm
+
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('events', 'totmiles')
+prob_set = predict(model, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Training set)',
+     xlab = 'Events', ylab = 'Totmiles',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'tomato', 'springgreen3'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'red3', 'green4'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('events', 'totmiles')
+prob_set = predict(model, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Test set)',
+     xlab = 'Events', ylab = 'Totmiles',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'tomato', 'springgreen3'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'red3', 'green4'))
+
